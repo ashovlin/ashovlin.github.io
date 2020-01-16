e77c2c0cf97198568e573a869b9bfbde3bc4c49e
Alex Shovlin <ashovlin@gmail.com>
2020-01-16 12:19:10 -0600

    Add CNAME

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ashovlin.github.io
+
+1. pelican content
+2. ghp-import output/
+3. git push git@github.com:ashovlin/ashovlin.github.io.git gh-pages:master
--- pelicanconf.py
+++ pelicanconf.py
@@ -34,3 +34,6 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
